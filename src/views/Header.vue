0d7f4925cb8fc804d663157bb06fd046afac73ea
Preeti <singhp1992@gmail.com>
2019-08-28 21:13:17 -0500

    first commit

---
+++ src/views/Header.vue
@@ -0,0 +1,41 @@
+// header here
+// add search option - bootstrap
+<template>
+  <div class="header" id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/products">Products</router-link> |
+    <router-link to="/cart">Shopping Cart</router-link>
+  </div>
+</template>Î
+
+<script> 
+export default {
+  name: "header",
+  components: {
+    
+  }
+};
+</script>
+
+<style>
+#nav {
+  padding: 30px;
+  border: 1px solid black;
+  background-color: black;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  color: white;
+}
+
+a {
+  text-decoration: none;
+}
+
+#nav a.router-link-exact-active {
+  color: #d878f0;
+}
+</style>
---
+++ src/views/Products.vue
@@ -0,0 +1,21 @@
+// all products here
+<template>
+  <div class="products">
+    <Title msg="All Products" />
+    <h1> Testing</h1>
+  </div>
+</template>
+
+<script>
+import Title from "@/components/Title.vue";
+
+export default {
+  name: "products",
+  components: {
+    Title
+  }
+};
+</script>
+
+<style>
+</style>
