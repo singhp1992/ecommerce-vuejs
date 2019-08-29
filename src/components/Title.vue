6b0bceb1ac0ca092d92b9de3fd1121966c317a68
Preeti <singhp1992@gmail.com>
2019-08-29 17:26:30 -0500

    adding padding top to h1 title

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -17,15 +17,22 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
+}
+
+h1 {
+  padding-top: 20px;
 }
 </style>
