3984e1bb2feffc6e6dfb1ec51dac245a2c4f481c
Preeti <singhp1992@gmail.com>
2019-08-29 17:32:01 -0500

    adding dark color scheme

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,7 +1,7 @@
 // header here
 // add search option - bootstrap
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <router-link to="/" class="navbar-brand" href="#">Store</router-link>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
