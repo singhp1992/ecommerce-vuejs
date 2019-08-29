8b2fc2152377c5fea826e4c056da65c8662c1154
Preeti <singhp1992@gmail.com>
2019-08-28 21:27:47 -0500

    importing bootstrap css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import BootstrapVue from 'bootstrap-vue'
 import App from "./App.vue";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from "./router";
 import "./registerServiceWorker";
 
