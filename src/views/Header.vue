a27d22f632c2ce3e9bb0f02e9e2be275332b6a1a
Preeti <singhp1992@gmail.com>
2019-08-29 17:00:05 -0500

    adding router link to store button

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -2,11 +2,10 @@
 // add search option - bootstrap
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Store</a>
+  <router-link class="navbar-brand" href="#">Store</router-link>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
