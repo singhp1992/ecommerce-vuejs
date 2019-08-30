40292603853221e9062634b81caa994770d4d30f
Preeti <singhp1992@gmail.com>
2019-08-30 16:46:11 -0500

    removing store href

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -2,7 +2,7 @@
 // add search option - bootstrap
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <router-link to="/" class="navbar-brand" href="#">Store</router-link>
+  <router-link to="/" class="navbar-brand">Store</router-link>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
