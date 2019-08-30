c2045cf1f2e2dcab7267dbe6660818b88feac793
Preeti <singhp1992@gmail.com>
2019-08-30 16:53:59 -0500

    adding div around nav

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,26 +1,28 @@
 // header here
 // add search option - bootstrap
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <router-link to="/" class="navbar-brand">Store</router-link>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <router-link to="/" class="nav-link">Home<span class="sr-only">(current)</span></router-link>
-      </li>
-      <li class="nav-item active">
-        <router-link to="/about" class="nav-link">About<span class="sr-only">(current)</span></router-link>
-      </li>
-      <li class="nav-item active">
-        <router-link to="/products" class="nav-link">Products<span class="sr-only">(current)</span></router-link>
-      </li>
-    </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
-    </div>
-</nav>
+  <div> 
+    <nav class="height navbar navbar-expand-lg navbar-dark bg-dark">
+    <router-link to="/" class="navbar-brand">Store</router-link>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <router-link to="/" class="nav-link">Home<span class="sr-only">(current)</span></router-link>
+        </li>
+        <li class="nav-item active">
+          <router-link to="/about" class="nav-link">About<span class="sr-only">(current)</span></router-link>
+        </li>
+        <li class="nav-item active">
+          <router-link to="/products" class="nav-link">Products<span class="sr-only">(current)</span></router-link>
+        </li>
+      </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+  </div>
 </template>Î
 
 <script> 
@@ -33,7 +35,7 @@ export default {
 </script>
 
 <style>
-.navbar {
-  padding: 1rem 1rem;
+.height {
+  padding: 2rem 1rem;
 }
 </style>
