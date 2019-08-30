4d957a603219b0e58a11758a5b7f939dc53cb155
Preeti <singhp1992@gmail.com>
2019-08-30 16:45:58 -0500

    removing home page href

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -9,7 +9,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <router-link to="/" class="nav-link" href="#">Home<span class="sr-only">(current)</span></router-link>
+        <router-link to="/" class="nav-link">Home<span class="sr-only">(current)</span></router-link>
       </li>
       <li class="nav-item active">
         <router-link to="/about" class="nav-link">About<span class="sr-only">(current)</span></router-link>
