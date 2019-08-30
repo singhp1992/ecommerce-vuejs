c1532bc820438aa477eca4c7147477aac2b5cb5e
Preeti <singhp1992@gmail.com>
2019-08-30 16:46:48 -0500

    removing unused button div

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -3,9 +3,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <router-link to="/" class="navbar-brand">Store</router-link>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
