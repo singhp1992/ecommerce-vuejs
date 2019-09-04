e8341f56943f2e65789785440858919c207f7d60
Preeti <singhp1992@gmail.com>
2019-09-03 19:56:30 -0500

    changed larger to purple on router link

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -3,7 +3,7 @@
 <template>
   <div id="height"> 
     <nav class="height navbar navbar-expand-lg navbar-dark bg-dark">
-    <router-link to="/" id="larger" class="navbar-brand">Store</router-link>
+    <router-link to="/" id="purple" class="navbar-brand">Store</router-link>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul id="purple" class="navbar-nav mr-auto">
         <li class="nav-item active">
